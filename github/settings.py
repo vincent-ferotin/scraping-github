93adc93986dfdad75723f122208a7523c00d573d
Vincent Férotin <vincent.ferotin@gmail.com>
2015-09-11 15:59:35 +0200

    Remove random variation in download delay.

--- github/settings.py
+++ github/settings.py
@@ -25,6 +25,8 @@ NEWSPIDER_MODULE = 'github.spiders'
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
 DOWNLOAD_DELAY = 1
+RANDOMIZE_DOWNLOAD_DELAY = False
+
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN=16
 #CONCURRENT_REQUESTS_PER_IP=16
